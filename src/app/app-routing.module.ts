cdfcdbd98078509d49ce3f469bc939d6df20b73e
Sergio Andres Valencia Yepes <svalenciay@psl.com.co>
2020-01-02 18:13:10 -0500

    Creacion de la configuracion inicial del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { PersonajesComponent } from './personajes/personajes.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PeliculasComponent },
+  { path: 'personajes', component: PersonajesComponent },
+  { path: '**',  redirectTo: '', component: PeliculasComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PeliculasComponent } from './peliculas/peliculas.component';
+import { PersonajesComponent } from './personajes/personajes.component';
+import { ListasComponent } from './listas/listas.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculasComponent,
+    PersonajesComponent,
+    ListasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
